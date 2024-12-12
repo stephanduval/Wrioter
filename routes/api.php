02a628fb41b00bbc5d8ad302d04279bc251dfc82
Stephan DuVal <stephan.duval@gmail.com>
2024-12-12 07:19:43 -0700

    Updated api.php for readability

--- routes/api.php
+++ routes/api.php
@@ -6,49 +6,51 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CompanyController;
 use App\Http\Controllers\RolesController;
-use App\Http\Controllers\UserRoleController;
-use App\Http\Controllers\UserCompanyController;
 use App\Http\Controllers\EmailController;
-
 
 /*
 |--------------------------------------------------------------------------
 | API Routes
 |--------------------------------------------------------------------------
 |
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
+| Register API routes for your application. These routes are loaded by the
+| RouteServiceProvider and assigned the "api" middleware group.
 |
 */
 
+// Authentication Routes
 Route::group(['prefix' => 'auth'], function () {
     Route::post('login', [AuthController::class, 'login']);
     Route::post('register', [AuthController::class, 'register']);
 
-    Route::group(['middleware' => 'auth:api'], function() {
-      Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:api');
-      Route::get('user', [AuthController::class, 'user']);
+    Route::middleware('auth:api')->group(function () {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::get('user', [AuthController::class, 'user']);
     });
 });
 
+// User Management Routes
 Route::middleware('auth:api')->group(function () {
     Route::get('/users', [UserController::class, 'index']);
-    Route::get('/email', [EmailController::class, 'index']);
+    Route::get('/users/{id}', [UserController::class, 'showUser']);
+    Route::post('/users', [UserController::class, 'addUser'])->name('users.store');
+    Route::put('/users/{id}', [UserController::class, 'updateUser']);
+    Route::delete('/users/{id}', [UserController::class, 'deleteUser']);
 });
 
+// Email Management Routes
+Route::middleware('auth:api')->get('/email', [EmailController::class, 'index']);
 
-Route::middleware('auth:api')->get('/companies/all', [CompanyController::class, 'allCompanies']);
-Route::middleware('auth:api')->get('/paginatedCompanies', [CompanyController::class, 'paginatedIndex']);
+// Company Management Routes
+Route::middleware('auth:api')->group(function () {
+    Route::get('/companies/all', [CompanyController::class, 'allCompanies']);
+    Route::get('/paginatedCompanies', [CompanyController::class, 'paginatedIndex']);
+});
 
-Route::middleware('auth:api')->delete('/users/{id}', [UserController::class, 'deleteUser']);
-
+// Role Management Routes
 Route::middleware('auth:api')->get('/roles', [RolesController::class, 'index']);
-Route::get('/users/{id}', [UserController::class, 'showUser']);
-Route::put('/users/{id}', [UserController::class, 'updateUser']);
 
-
-Route::post('/users', [UserController::class, 'addUser'])->name('users.store');
+// Fallback Route
 Route::fallback(function () {
-    return view('welcome'); // This could be causing the HTML response.
+    return response()->json(['message' => 'Resource not found.'], 404);
 });
