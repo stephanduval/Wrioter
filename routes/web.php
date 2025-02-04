d0f4703a2adb4f0c0327ce3c8cbbef82ea52debb
Stephan <stephan.duval@gmail.com>
2025-02-04 17:34:59 +0000

    fixed the url issue with a laravel route solution

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    if (auth()->check()) {
+        return redirect('/dashboards/crm'); // If user is logged in, send to dashboard
+    }
+    return redirect('/login?redirect=/dashboards/crm');
+});
+
+
+
 Route::get('{any?}', function() {
     return view('application');
 })->where('any', '.*');
