01e0cd8ff399a01787c8a9c250a7e0b412d86d36
StephanDuVal <stephanduval@gmail.com>
2025-04-14 12:18:06 -0600

    Modified the database seeder file to removed the deleted MessageSeeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -12,7 +12,6 @@ class DatabaseSeeder extends Seeder
             RolePermissionActionSubjectSeeder::class,
             CompanySeeder::class,
             UserSeeder::class,
-            MessageSeeder::class,
             // ... other seeders
         ]);
     }
