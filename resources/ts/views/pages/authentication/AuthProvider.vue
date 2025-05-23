4f9f7050943a2775c77ba3df546b8491736f5b94
StephanDuVal <stephanduval@gmail.com>
2025-05-22 23:14:31 -0600

    deleted social media icons from the login page

--- resources/ts/views/pages/authentication/AuthProvider.vue
+++ resources/ts/views/pages/authentication/AuthProvider.vue
@@ -1,46 +1,7 @@
 <script setup lang="ts">
-import { useTheme } from 'vuetify'
-
-const { global } = useTheme()
-
-const authProviders = [
-  {
-    icon: 'bx-bxl-facebook-circle',
-    color: '#4267b2',
-    colorInDark: '#497CE2',
-  },
-  {
-    icon: 'bx-bxl-twitter',
-    color: '#1da1f2',
-    colorInDark: '#1da1f2',
-  },
-  {
-    icon: 'bx-bxl-github',
-    color: '#272727',
-    colorInDark: '#fff',
-  },
-  {
-    icon: 'bx-bxl-google',
-    color: '#dd4b39',
-    colorInDark: '#db4437',
-  },
-]
+// Component intentionally left empty to remove social media icons
 </script>
 
 <template>
-  <div class="d-flex justify-center flex-wrap gap-1">
-    <VBtn
-      v-for="link in authProviders"
-      :key="link.icon"
-      icon
-      variant="text"
-      size="small"
-      :color="global.name.value === 'dark' ? link.colorInDark : link.color"
-    >
-      <VIcon
-        size="20"
-        :icon="link.icon"
-      />
-    </VBtn>
-  </div>
+  <!-- Empty template to remove social media icons -->
 </template>
