f0fab12d268d9ef35bd423099fd47468c98949cd
StephanDuVal <stephan.duval@gmail.com>
2025-10-07 08:34:05 -0600

    added scheduler to clean up old scrivener import files

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,15 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Schedule;
 
+// Scheduled tasks (Laravel 11 style)
+Schedule::command('scrivener:cleanup')
+    ->daily()
+    ->at('02:00')
+    ->appendOutputTo(storage_path('logs/cleanup.log'));
+
+// Console commands
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote')->hourly();
+})->purpose('Display an inspiring quote');
